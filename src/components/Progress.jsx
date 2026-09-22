7b70ee3a0e68f2202a5063744c1a2f07642035e6
Rafiullah-Najat <rafiullahnajat20@gmail.com>
2026-09-22 23:27:36 +0430

    feat: add Progress component

---
+++ src/components/Progress.jsx
@@ -0,0 +1,24 @@
+export default function Progress({
+  index,
+  numQuestions,
+  points,
+  maxPossiblePionts,
+  answer,
+}) {
+  return (
+    <>
+      <header className="progress">
+        <progress
+          max={numQuestions}
+          value={index + Number(answer !== null)}
+        ></progress>
+        <p>
+          Questions <strong>{index + 1}</strong> / {numQuestions}
+        </p>
+        <p>
+          <strong>{points}</strong> / {maxPossiblePionts}
+        </p>
+      </header>
+    </>
+  );
+}
