349800c26aad4d5093ae0b841752adba4199c084
Rafiullah-Najat <rafiullahnajat20@gmail.com>
2026-09-26 13:12:15 +0430

    feat: implement FinishScreen component

---
+++ src/components/FinishScreen.jsx
@@ -0,0 +1,29 @@
+export default function FinishScreen({
+  points,
+  maxPossiblePionts,
+  highScore,
+  dispatch,
+}) {
+  const percentage = (points / maxPossiblePionts) * 100;
+  let emoji;
+  if (percentage === 100) emoji = "🥇";
+  if (percentage >= 80 && percentage < 100) emoji = "🎉";
+  if (percentage >= 50 && percentage < 80) emoji = "😊";
+  if (percentage >= 0 && percentage < 50) emoji = "😐";
+  if (percentage === 0) emoji = "🤦‍♂️";
+  return (
+    <>
+      <p className="result">
+        <span>{emoji}</span> You scored <strong>{points}</strong> out of{" "}
+        {maxPossiblePionts} ({Math.ceil(percentage)}%)
+      </p>
+      <p className="highscore">(Highscore: {highScore} points)</p>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "Restart" })}
+      >
+        Restart
+      </button>
+    </>
+  );
+}
